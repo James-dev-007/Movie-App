ad14bf6a9e0807599ee98b9e58dff6a8608f82b5
James-dev-007 <msizajst@gmail.com>
2025-06-17 16:27:54 +0200

    first commit

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from '../components/Hero'
+import Featured from '../components/Featured'
+import Trailers from '../components/Trailers'
+
+const Home = () => {
+    return (
+        <>
+            <Hero />
+            <Featured />
+            <Trailers />
+        </>
+    )
+}
+
+export default Home
